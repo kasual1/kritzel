246b188bca56ff4b2a332251ceccf47f6621ab52
Lukas Seifert <seifertluk11@gmail.com>
2024-11-02 12:23:35 +0100

    WIP Test creation of components via stencil

--- packages/angular/projects/lib/src/lib/index.ts
+++ packages/angular/projects/lib/src/lib/index.ts
@@ -2,5 +2,6 @@
 import * as d from './proxy';
 
 export const DIRECTIVES = [
+  d.MyButton,
   d.MyComponent
 ];
--- packages/angular/projects/lib/src/lib/proxy.ts
+++ packages/angular/projects/lib/src/lib/proxy.ts
@@ -8,6 +8,27 @@ import { Components } from '@workspace/stencil';
 
 
 @ProxyCmp({
+})
+@Component({
+  selector: 'my-button',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
+  inputs: [],
+})
+export class MyButton {
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
+}
+
+
+export declare interface MyButton extends Components.MyButton {}
+
+
+@ProxyCmp({
   inputs: ['age', 'first', 'last']
 })
 @Component({
