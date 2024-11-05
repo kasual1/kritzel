8252ce589d15e7070504807f828dfd5a0b7a0f06
kasual1 <seifertluk11@gmail.com>
2024-11-05 09:08:28 +0100

    Add vue lib

--- packages/libs/stencil/stencil.config.ts
+++ packages/libs/stencil/stencil.config.ts
@@ -1,6 +1,7 @@
 import { Config } from '@stencil/core';
 import { angularOutputTarget } from '@stencil/angular-output-target';
 import { reactOutputTarget } from '@stencil/react-output-target';
+import { vueOutputTarget } from '@stencil/vue-output-target';
 
 export const config: Config = {
   namespace: 'stencil',
@@ -16,17 +17,21 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null,
     },
+    { type: 'dist-custom-elements', externalRuntime: false },
     angularOutputTarget({
       componentCorePackage: '@workspace/stencil',
-      directivesProxyFile: '../../packages/angular/projects/lib/src/lib/proxy.ts',
-      directivesArrayFile: '../../packages/angular/projects/lib/src/lib/index.ts',
+      directivesProxyFile: '../angular/projects/lib/src/lib/proxy.ts',
+      directivesArrayFile: '../angular/projects/lib/src/lib/index.ts',
       outputType: 'component'
     }),
     reactOutputTarget({
       outDir: '../react/lib/components/stencil-generated/',
       stencilPackageName: '../../../../stencil',
     }),
-    { type: 'dist-custom-elements', externalRuntime: false },
+    vueOutputTarget({
+      componentCorePackage: '@workspace/stencil',
+      proxiesFile: '../vue/lib/components.ts',
+    }),
   ],
   testing: {
     browserHeadless: "new",
--- packages/packages/angular/projects/lib/src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-
-import * as d from './proxy';
-
-export const DIRECTIVES = [
-  d.MyButton,
-  d.MyComponent,
-  d.MyList,
-  d.MyTitle
-];
