b4919c2f53ab472d5923b440bd85d5060f3a5b13
kasual1 <seifertluk11@gmail.com>
2024-11-07 00:54:42 +0100

    Improve stencil setup

--- packages/libs/vue/lib/components.ts
+++ packages/libs/vue/lib/components.ts
@@ -6,6 +6,8 @@ import { defineContainer } from './vue-component-lib/utils';
 import type { JSX } from '@workspace/stencil';
 
 
+
+
 export const MyButton = /*@__PURE__*/ defineContainer<JSX.MyButton>('my-button', undefined);
 
 
