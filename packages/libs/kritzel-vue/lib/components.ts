6d73b1fcce1dbd5cd5ebd4edd2af146678fcde4f
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-04-13 08:23:59 +0200

    #22: Fix vue build process

--- packages/libs/kritzel-vue/lib/components.ts
+++ packages/libs/kritzel-vue/lib/components.ts
@@ -1,25 +1,28 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated vue proxies */
-import { defineContainer } from './vue-component-lib/utils';
+import { defineContainer, type StencilVueComponent } from '@stencil/vue-output-target/runtime';
 
-import type { JSX } from '@kritzel/stencil';
+import type { JSX } from '../../kritzel-stencil/dist/components';
 
-
-
-
-export const KritzelControls = /*@__PURE__*/ defineContainer<JSX.KritzelControls>('kritzel-controls', undefined);
-
-
-export const KritzelEditor = /*@__PURE__*/ defineContainer<JSX.KritzelEditor>('kritzel-editor', undefined);
+import { defineCustomElement as defineKritzelControls } from '../../kritzel-stencil/dist/components/kritzel-controls.js';
+import { defineCustomElement as defineKritzelEditor } from '../../kritzel-stencil/dist/components/kritzel-editor.js';
+import { defineCustomElement as defineKritzelEngine } from '../../kritzel-stencil/dist/components/kritzel-engine.js';
+import { defineCustomElement as defineKritzelIcon } from '../../kritzel-stencil/dist/components/kritzel-icon.js';
 
 
-export const KritzelEngine = /*@__PURE__*/ defineContainer<JSX.KritzelEngine>('kritzel-engine', undefined, [
+export const KritzelControls: StencilVueComponent<JSX.KritzelControls> = /*@__PURE__*/ defineContainer<JSX.KritzelControls>('kritzel-controls', defineKritzelControls);
+
+
+export const KritzelEditor: StencilVueComponent<JSX.KritzelEditor> = /*@__PURE__*/ defineContainer<JSX.KritzelEditor>('kritzel-editor', defineKritzelEditor);
+
+
+export const KritzelEngine: StencilVueComponent<JSX.KritzelEngine> = /*@__PURE__*/ defineContainer<JSX.KritzelEngine>('kritzel-engine', defineKritzelEngine, [
   'activeTool'
 ]);
 
 
-export const KritzelIcon = /*@__PURE__*/ defineContainer<JSX.KritzelIcon>('kritzel-icon', undefined, [
+export const KritzelIcon: StencilVueComponent<JSX.KritzelIcon> = /*@__PURE__*/ defineContainer<JSX.KritzelIcon>('kritzel-icon', defineKritzelIcon, [
   'name',
   'label'
 ]);
