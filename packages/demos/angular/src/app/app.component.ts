f63bce095e1b7df058496f0c31edf6997d4b6ce9
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-04-13 12:41:05 +0200

    #22: WIP Update angula demo

--- packages/demos/angular/src/app/app.component.ts
+++ packages/demos/angular/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LibModule } from 'kritzel-angular';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, LibModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [CommonModule, LibModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular';
