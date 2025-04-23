09e67590586e54295acfaa3e68b56308b43e9108
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-04-23 08:25:46 +0200

    #9: WIP Implement eraser tool for touch events

--- packages/libs/kritzel-stencil/src/classes/tools/eraser-tool.class.ts
+++ packages/libs/kritzel-stencil/src/classes/tools/eraser-tool.class.ts
@@ -53,4 +53,42 @@ export class KritzelEraserTool extends KritzelBaseTool {
     }
   }
 
+  handleTouchStart(_event: TouchEvent): void {
+      this._store.state.isErasing = true;
+  }
+
+  handleTouchMove(event: TouchEvent): void {
+    if (this._store.state.isErasing) {
+      const path = event.composedPath() as HTMLElement[];
+      const selectedObject = path.find(element => element.classList && element.classList.contains('object'));
+
+      if (selectedObject) {
+        for (const object of this._store.allObjects) {
+          if (selectedObject.id === object.id) {
+            object.markedForRemoval = true;
+          }
+        }
+      }
+
+      this._store.rerender();
+    }
+  }
+
+  handleTouchEnd(_event: TouchEvent): void {
+    if(this._store.state.isErasing) {
+      const removeCommands = this._store.allObjects
+        .filter(object => object.markedForRemoval)
+        .map(object => {
+          object.markedForRemoval = false;
+          return new RemoveObjectCommand(this._store, this, object);
+        });
+
+      if(removeCommands.length > 0) {
+        this._store.history.executeCommand(new BatchCommand(this._store, this, removeCommands));
+      }
+
+      this._store.state.isErasing = false;
+    }
+  }
+
 }
