512e9c187cbeb3065cd87d92369cd2a1b22f7002
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-04-24 08:37:14 +0200

    #9: Refactor eraser tool to use elementFromPoint instead of composedPath

--- packages/libs/kritzel-stencil/src/classes/tools/eraser-tool.class.ts
+++ packages/libs/kritzel-stencil/src/classes/tools/eraser-tool.class.ts
@@ -1,14 +1,14 @@
-import { KritzelClickHelper } from "../../helpers/click.helper";
-import { KritzelStore } from "../store.class";
-import { BatchCommand } from "../commands/batch.command";
-import { RemoveObjectCommand } from "../commands/remove-object.command";
-import { KritzelBaseTool } from "./base-tool.class";
+import { KritzelClickHelper } from '../../helpers/click.helper';
+import { KritzelStore } from '../store.class';
+import { BatchCommand } from '../commands/batch.command';
+import { RemoveObjectCommand } from '../commands/remove-object.command';
+import { KritzelBaseTool } from './base-tool.class';
 
 export class KritzelEraserTool extends KritzelBaseTool {
   name: string = 'eraser';
   icon: string = 'eraser';
 
-  constructor(store: KritzelStore){
+  constructor(store: KritzelStore) {
     super(store);
   }
 
@@ -20,24 +20,28 @@ export class KritzelEraserTool extends KritzelBaseTool {
 
   handleMouseMove(event: MouseEvent): void {
     if (this._store.state.isErasing) {
-        const path = event.composedPath() as HTMLElement[];
-        const selectedObject = path.find(element => element.classList && element.classList.contains('object'));
+      const shadowRoot = this._store.state.host?.shadowRoot;
+      if (!shadowRoot) return;
 
-        if (selectedObject) {
-          for (const object of this._store.allObjects) {
-            if (selectedObject.id === object.id) {
-              object.markedForRemoval = true;
-            }
+      const elementAtPoint = shadowRoot.elementFromPoint(event.clientX, event.clientY);
+      if (!elementAtPoint) return;
+
+      const selectedObject = elementAtPoint.closest('.object') as HTMLElement | null;
+
+      if (selectedObject) {
+        for (const object of this._store.allObjects) {
+          if (selectedObject.id === object.id) {
+            object.markedForRemoval = true;
           }
         }
+      }
 
-        this._store.rerender();
+      this._store.rerender();
     }
   }
 
   handleMouseUp(_event: MouseEvent): void {
-    if(this._store.state.isErasing) {
-
+    if (this._store.state.isErasing) {
       const removeCommands = this._store.allObjects
         .filter(object => object.markedForRemoval)
         .map(object => {
@@ -45,7 +49,7 @@ export class KritzelEraserTool extends KritzelBaseTool {
           return new RemoveObjectCommand(this._store, this, object);
         });
 
-      if(removeCommands.length > 0) {
+      if (removeCommands.length > 0) {
         this._store.history.executeCommand(new BatchCommand(this._store, this, removeCommands));
       }
 
@@ -54,13 +58,19 @@ export class KritzelEraserTool extends KritzelBaseTool {
   }
 
   handleTouchStart(_event: TouchEvent): void {
-      this._store.state.isErasing = true;
+    this._store.state.isErasing = true;
   }
 
   handleTouchMove(event: TouchEvent): void {
     if (this._store.state.isErasing) {
-      const path = event.composedPath() as HTMLElement[];
-      const selectedObject = path.find(element => element.classList && element.classList.contains('object'));
+      const shadowRoot = this._store.state.host?.shadowRoot;
+      if (!shadowRoot) return;
+
+      const touch = event.touches[0];
+      const elementAtPoint = shadowRoot.elementFromPoint(touch.clientX, touch.clientY);
+      if (!elementAtPoint) return;
+
+      const selectedObject = elementAtPoint.closest('.object') as HTMLElement | null;
 
       if (selectedObject) {
         for (const object of this._store.allObjects) {
@@ -75,7 +85,7 @@ export class KritzelEraserTool extends KritzelBaseTool {
   }
 
   handleTouchEnd(_event: TouchEvent): void {
-    if(this._store.state.isErasing) {
+    if (this._store.state.isErasing) {
       const removeCommands = this._store.allObjects
         .filter(object => object.markedForRemoval)
         .map(object => {
@@ -83,12 +93,11 @@ export class KritzelEraserTool extends KritzelBaseTool {
           return new RemoveObjectCommand(this._store, this, object);
         });
 
-      if(removeCommands.length > 0) {
+      if (removeCommands.length > 0) {
         this._store.history.executeCommand(new BatchCommand(this._store, this, removeCommands));
       }
 
       this._store.state.isErasing = false;
     }
   }
-
 }
