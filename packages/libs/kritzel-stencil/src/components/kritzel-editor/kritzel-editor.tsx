b8920f0f95550afbd9452f57440fd0287219aef3
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-05-03 16:33:17 +0200

    #21: Remove demo data

--- packages/libs/kritzel-stencil/src/components/kritzel-editor/kritzel-editor.tsx
+++ packages/libs/kritzel-stencil/src/components/kritzel-editor/kritzel-editor.tsx
@@ -1,8 +1,6 @@
 import { Component, Host, Prop, State, h} from '@stencil/core';
 import { KritzelIconRegistry } from '../../classes/icon-registry.class';
 import { KritzelToolbarControl } from '../../components';
-import { KritzelSelectionTool } from '../../classes/tools/selection-tool.class';
-import { KritzelBrushTool } from '../../classes/tools/brush-tool.class';
 
 @Component({
   tag: 'kritzel-editor',
@@ -12,34 +10,10 @@ import { KritzelBrushTool } from '../../classes/tools/brush-tool.class';
 export class KritzelEditor {
 
   @Prop()
-  controls: KritzelToolbarControl[] = [
-    {
-      name: 'selection',
-      type: 'tool',
-      tool: KritzelSelectionTool,
-      icon: 'heart',
-    },
-    {
-      name: 'brush',
-      type: 'tool',
-      tool: KritzelBrushTool,
-      icon: 'pen',
-      isDefault: true,
-      config: {
-        color: '#000000',
-        size: 24,
-      },
-    },
-    {
-      name: 'config',
-      type: 'config',
-    }
-  ]
+  controls: KritzelToolbarControl[];
 
   @Prop()
-  customSvgIcons: Record<string, string> = {
-    'heart': `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>`
-  };
+  customSvgIcons: Record<string, string>;
 
   @State()
   activeControl: string | null = null;
