242e5227acd6c85a6607eabd8e24393f533cd7ce
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-05-16 02:15:12 +0200

    #21: WIP Fix accidental gesture triggering on iOS

--- packages/libs/kritzel-stencil/src/components/ui/kritzel-utility-panel/kritzel-utility-panel.tsx
+++ packages/libs/kritzel-stencil/src/components/ui/kritzel-utility-panel/kritzel-utility-panel.tsx
@@ -6,7 +6,6 @@ import { Component, Host, h, Event, EventEmitter } from '@stencil/core';
   shadow: true,
 })
 export class KritzelUtilityPanel {
-
   @Event()
   undo: EventEmitter<void>;
 
@@ -16,22 +15,33 @@ export class KritzelUtilityPanel {
   @Event()
   delete: EventEmitter<void>;
 
+  handleUndo(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.undo.emit();
+  }
+
+  handleRedo(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.redo.emit();
+  }
 
   render() {
     return (
       <Host>
-          <button class="utility-button" onClick={() => this.undo.emit()}>
-            <kritzel-icon name="undo"></kritzel-icon>
-          </button>
-          <button class="utility-button" onClick={() => this.redo.emit()}>
-            <kritzel-icon name="redo"></kritzel-icon>
-          </button>
+        <button class="utility-button" onClick={(event) => this.handleUndo(event)}>
+          <kritzel-icon name="undo"></kritzel-icon>
+        </button>
+        <button class="utility-button" onClick={(event) => this.handleRedo(event)}>
+          <kritzel-icon name="redo"></kritzel-icon>
+        </button>
 
-          <div class="utility-separator"></div>
+        <div class="utility-separator"></div>
 
-          <button class="utility-button">
-            <kritzel-icon name="delete" onClick={() => this.delete.emit()}></kritzel-icon>
-          </button>
+        <button class="utility-button">
+          <kritzel-icon name="delete" onClick={() => this.delete.emit()}></kritzel-icon>
+        </button>
       </Host>
     );
   }
