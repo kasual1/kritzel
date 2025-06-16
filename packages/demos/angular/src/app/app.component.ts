c44fdc54f6e457e4e79e2e3441a7f2974c2708b1
Lukas Mühlenbeck <lukas.seifert@e-trusco.de>
2025-06-16 09:59:44 +0200

    Dynamically adjust editor-height to window

--- packages/demos/angular/src/app/app.component.ts
+++ packages/demos/angular/src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { KritzelEditor } from 'kritzel-angular';
 @Component({
-    selector: 'app-root',
-    imports: [KritzelEditor],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    standalone: true,
+  selector: 'app-root',
+  imports: [KritzelEditor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
-export class AppComponent{
-  title = 'angular';
+export class AppComponent implements OnInit{
+  @ViewChild('editor')
+  editor!: HTMLElement;
+
+  ngOnInit(): void {
+    document.body.style.height = window.innerHeight + 'px';
+    const editor = document.getElementById('editor');
+
+    if(editor){
+      editor.style.height = window.innerHeight + 'px';
+      editor.style.width = window.innerWidth + 'px';
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    document.body.style.height = window.innerHeight + 'px';
+    const editor = document.getElementById('editor');
+
+    if(editor){
+      editor.style.height = window.innerHeight + 'px';
+      editor.style.width = window.innerWidth + 'px';
+    }
+  }
 }
