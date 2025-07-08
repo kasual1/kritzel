ad5b3088792ad8aad5d7658a9c7c7123ee73aec7
kasual1 <seifertluk11@gmail.com>
2025-07-08 08:49:18 +0200

    #11: Comment out infos in docs

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -27,14 +27,14 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        {/* <Heading as="h1" className={clsx('hero__title', styles.heroTitle)}>
+        <Heading as="h1" className={clsx('hero__title', styles.heroTitle)}>
           Kritzel Documentation
           <br />
         </Heading>
          <p className={clsx('hero__subtitle', styles.heroSubtitle)}>
           Coming soon!
-        </p> */}
-        <Heading as="h1" className={clsx('hero__title', styles.heroTitle)}>
+        </p>
+        {/* <Heading as="h1" className={clsx('hero__title', styles.heroTitle)}>
           Infinite Canvas,
           <br />
           unbound creativity.
@@ -54,7 +54,7 @@ function HomepageHeader() {
               <ArrowRight style={{ width: '24px', height: '24px' }} />
             </span>
           </Link> 
-        </div>
+        </div> */}
        
       </div>
     </header>
